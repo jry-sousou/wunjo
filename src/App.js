47df10f5163cc3ce8024fd808f932a33c304667a
NICOLAS Jerry <58029811+jry-sousou@users.noreply.github.com>
2020-05-20 13:13:44 +0200

    front-calendrier

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Calendar from "./components/Calendar";
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    currentMonth: new Date(),
+    selectedDate: new Date()
+};
+
+renderHeader() {}
+
+  renderDays() {}
+
+  renderCells() {}
+
+  onDateClick = day => {};
+
+  nextMonth = () => {};
+
+  prevMonth = () => {};
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              Wunjo<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
